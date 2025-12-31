0864b1598aad18b1460e1da686f2ff5e9f7e1cef
christophernichols <chrisnichols17@gmail.com>
2025-12-30 20:06:56 -0600

    Update action plan UI: B.O.B. message and Central Time display

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5007, debug=True)
+    app.run(host='0.0.0.0', port=5006, debug=True)
